4ec554e018a36a5d8a1f36d279fefa7719e923c2
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-09-21 00:18:34 +0700

    toi uu

--- english-topic-web/src/App.jsx
+++ english-topic-web/src/App.jsx
@@ -1,16 +1,10 @@
-import { Outlet } from 'react-router-dom';
-import Footer from './Layouts/Footer';
-import Header from './Layouts/Header';
+
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <main className="main-content">
-        <Outlet />
-      </main>
-      <Footer />
+
     </div>
   );
 }
--- english-topic-web/src/Router/index.jsx
+++ english-topic-web/src/Router/index.jsx
@@ -1,36 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import About from '../Pages/About';
-import Home from '../Pages/Home';
-import ListTopic from '../Pages/List-Topic';
-import Practice from '../Pages/Practice';
-import Topic from '../Pages/Topic';
+// import { lazy } from "react";
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/topics',
-        element: <ListTopic />,
-      },
-      {
-        path: '/topic/:topicName',
-        element: <Topic />,
-      },
-      {
-        path: '/practice/:topicName',
-        element: <Practice />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-    ],
-  },
-]);
+
+
+export const publicRoutes = [
+
+];
+
+export const privateRoutes = [
+  // Có thể thêm các routes dành riêng cho admin sau này
+];
