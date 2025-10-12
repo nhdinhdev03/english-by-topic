93c09f21e71af6c925facfc8f6a70f98b5f21256
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-10-13 00:29:53 +0700

    tối ưu

--- english-topic-web/src/App.jsx
+++ english-topic-web/src/App.jsx
@@ -1,14 +1,18 @@
 import { Suspense } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Footer from "./Layouts/Footer";
-import Header from "./Layouts/Header";
-import { allRoutes } from "./Router";
+import BottomNav from "./components/BottomNav";
 import ScrollToTop from "./components/Scroll/ScrollToTop/ScrollToTop";
 import ScrollToTopOnNavigate from "./components/Scroll/ScrollToTopOnNavigate/ScrollToTopOnNavigate";
+import { ToastContainer } from "./components/UI";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { ProgressProvider } from "./contexts/ProgressContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { ToastProvider } from "./contexts/ToastContext";
+import Footer from "./Layouts/Footer";
+import Header from "./Layouts/Header";
+import { allRoutes } from "./Router";
 import "./styles/App.scss";
+import "./styles/enhanced-animations.scss";
 
 // Loading component
 const Loading = () => (
@@ -21,35 +25,41 @@ const Loading = () => (
 function App() {
   return (
     <ThemeProvider>
-      <LanguageProvider>
-        <ProgressProvider>
-          <Router>
-            <div className="app">
-              <Suspense fallback={<Loading />}>
-                <Header />
-              <main className="main-content">
-                <Routes>
-                  {allRoutes.map((route, index) => {
-                    const Component = route.element;
-                    return (
-                      <Route
-                        key={index}
-                        path={route.path}
-                        element={<Component />}
-                      />
-                    );
-                  })}
-                </Routes>
-              </main>
-              <Footer />
-              {/* Scroll components */}
-              <ScrollToTopOnNavigate />
-              <ScrollToTop />
-            </Suspense>
-          </div>
-        </Router>
-      </ProgressProvider>
-      </LanguageProvider>
+      <ToastProvider>
+        <LanguageProvider>
+          <ProgressProvider>
+            <Router>
+              <div className="app">
+                <Suspense fallback={<Loading />}>
+                  <Header />
+                  <main className="main-content">
+                    <Routes>
+                      {allRoutes.map((route, index) => {
+                        const Component = route.element;
+                        return (
+                          <Route
+                            key={index}
+                            path={route.path}
+                            element={<Component />}
+                          />
+                        );
+                      })}
+                    </Routes>
+                  </main>
+                  <Footer />
+                  {/* Mobile bottom navigation */}
+                  <BottomNav />
+                  {/* Scroll components */}
+                  <ScrollToTopOnNavigate />
+                  <ScrollToTop />
+                  {/* Toast notifications */}
+                  <ToastContainer />
+                </Suspense>
+              </div>
+            </Router>
+          </ProgressProvider>
+        </LanguageProvider>
+      </ToastProvider>
     </ThemeProvider>
   );
 }
